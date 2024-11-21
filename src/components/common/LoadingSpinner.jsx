a3b6b14c30ec1ba0ffa92b3c7fe9320d12bca66a
Osama S <79966712+samasthtc@users.noreply.github.com>
2024-11-21 12:56:24 +0200

    made loading spinner a component

---
+++ src/components/common/LoadingSpinner.jsx
@@ -0,0 +1,7 @@
+export default function LoadingSpinner() {
+  return (
+    <div className="spinner-border text-primary text-center" role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+}
