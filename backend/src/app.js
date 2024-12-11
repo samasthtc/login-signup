97982a0bd5165fbcfcca5112ac98b11927602165
samasthtc <samasthtc@gmail.com>
2024-12-11 23:41:35 +0200

    modified cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,4 +23,6 @@ app.use(errorHandler);
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Backend server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Backend server running on port ${PORT}`)
+);
